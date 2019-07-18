41605762ddce706ba592ed526c6764e48fd4ecaf
Omar Romaña <olcr1996@gmail.com>
2019-07-17 20:43:22 -0500

    Commit inicial.

---
+++ web/modules/custom/my_module/src/Controller/MyModuleController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Drupal\my_module\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class MyModuleController extends ControllerBase {
+
+/* Método acción content devuelve directamente un contenido en html,
+este método será llamado por la ruta "my_module.routign.yml" */
+  public function content() {
+    return array(
+        '#type' => 'markup',
+        '#markup' => $this->t('Módulo dummy Hola mundo'),
+    );
+  }
+
+}
+
+?>
